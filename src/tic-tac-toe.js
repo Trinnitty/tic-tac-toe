797ab71f5a94d331a44e2541521eb8e783c4f0e3
Tatsiana Bukzheleva <ledimersedes@yandex.ru>
2017-09-22 12:40:52 +0300

    5 solved

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -6,7 +6,9 @@ class TicTacToe {
             [null,null,null]
         ];  
         this.player = 'x'; 
-        this.win = null;   
+        this.win = null; 
+        this.Turns = false;
+        this.finish = false;
     }
 
     getCurrentPlayerSymbol() {
@@ -22,15 +24,8 @@ class TicTacToe {
             }else {
                 this.player ='o';
             }
-        } 
-    }
-
-    isFinished() {
-           
-    }
-
-    getWinner() {
-            if( this.arr[0][0]==this.arr[0][1]&&this.arr[0][0]==this.arr[0][2]&&this.arr[0][2]!=null ||
+        }
+        if( this.arr[0][0]==this.arr[0][1]&&this.arr[0][0]==this.arr[0][2]&&this.arr[0][2]!=null ||
             this.arr[0][0]==this.arr[1][0]&&this.arr[1][0]==this.arr[2][0]&&this.arr[2][0]!=null ||
             this.arr[0][0]==this.arr[1][1]&&this.arr[0][0]==this.arr[2][2]&&this.arr[2][2]!=null ||
             this.arr[0][1]==this.arr[1][1]&&this.arr[0][1]==this.arr[2][1]&&this.arr[2][1]!=null ||
@@ -41,11 +36,40 @@ class TicTacToe {
                 if(this.player=='o'){
                     this.win ='x';
                 }else { this.win = 'o';}
-            };
+            }
+        for(var i=0; i<3; i++){
+            for(var j=0; j<3; j++){
+                if(this.arr[i][j]==null){
+                    this.Turns = false;
+                    return  this.Turns;
+                }
+            }
+        }
+        this.Turns = true;
+    }
+
+    isFinished() {
+        if(this.win!=null){
+            this.finish = true;
+        };
+        if(this.Turns){
+            this.finish  = true;
+        }
+        return this.finish;
+    }
+
+    getWinner() {
+            
         return this.win;    
     }
 
     noMoreTurns() {
+            
+        return  this.Turns;
+    }
+
+    isDraw() {
+        var turn = function(){
             for(var i=0; i<3; i++){
             for(var j=0; j<3; j++){
                 if(this.arr[i][j]==null){
@@ -54,14 +78,31 @@ class TicTacToe {
             }
         }
         return true;
-    }
-
-    isDraw() {
-
+        };
+        var win = function(){
+        if( this.arr[0][0]==this.arr[0][1]&&this.arr[0][0]==this.arr[0][2]&&this.arr[0][2]!=null ||
+            this.arr[0][0]==this.arr[1][0]&&this.arr[1][0]==this.arr[2][0]&&this.arr[2][0]!=null ||
+            this.arr[0][0]==this.arr[1][1]&&this.arr[0][0]==this.arr[2][2]&&this.arr[2][2]!=null ||
+            this.arr[0][1]==this.arr[1][1]&&this.arr[0][1]==this.arr[2][1]&&this.arr[2][1]!=null ||
+            this.arr[1][0]==this.arr[1][1]&&this.arr[1][0]==this.arr[1][2]&&this.arr[1][2]!=null ||
+            this.arr[0][2]==this.arr[1][1]&&this.arr[0][2]==this.arr[2][0]&&this.arr[2][0]!=null ||
+            this.arr[2][0]==this.arr[2][1]&&this.arr[2][0]==this.arr[2][2]&&this.arr[2][2]!=null ||
+            this.arr[0][2]==this.arr[1][2]&&this.arr[1][2]==this.arr[2][2]&&this.arr[2][2]!=null)
+            {
+                return true;
+            }else {return false;}   
+        }
+        // if(!win&&!turn){
+        //     return false;
+        // }
+        if(win){
+            return false;
+        }
+        if(turn){
+            return true;}    
     }
 
     getFieldValue(rowIndex, colIndex) {
-
         return this.arr[rowIndex][colIndex];
     }
 }
